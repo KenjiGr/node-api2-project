60bc121b4e1685c62da5f7971bc435c4a30de2d7
Kenji Greene <kenjigrr@gmail.com>
2021-11-30 23:52:06 -0600

    added update post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -42,6 +42,23 @@ router.post('/', async (req, res) => {
     }
 })
 // 4 PUT /api/posts/:id Updates the post with the specified id using data from the request body and **returns the modified document**, not the original
+router.put('/:id', async (req, res) => {
+    try{
+        if(!req.body.contents || !req.body.title){
+            res.status(400).json({ message: "Please provide title and contents for the post" });
+        }else{
+            Post.update(req.params.id, req.body).then(post => {
+                if(!post){
+                    res.status(404).json({ message: "The post with the specified ID does not exist" });
+                }else{
+                    res.status(200).json(post);
+                }
+            })
+        }
+    }catch (err){
+        res.status(500).json({ message: "The post information could not be modified" });
+    }
+})
 // 5 DELETE /api/posts/:id  Removes the post with the specified id and returns the **deleted post object**                                                  |
 //6 GET /api/posts/:id/comments Returns an **array of all the comment objects** associated with the post with the specified id      
 module.exports = router
